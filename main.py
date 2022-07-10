d2f10b87f5100caf32c8a480f688be53ed8d0061
elizabethrsotomayor <elizabethrsotomayor@gmail.com>
2022-07-10 11:42:16 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+def enough(cap: int, on: int, wait: int):
+    """
+    Bob is working as a bus driver. However, he has become extremely popular amongst the city's residents. With so
+    many passengers wanting to get aboard his bus, he sometimes has to face the problem of not enough space left on
+    the bus! He wants you to write a simple program telling him if he will be able to fit all the passengers. :param
+    cap: :param
+    on: :param
+    wait: :return:
+    """
+    return 0 if on + wait <= cap else abs(cap - (on + wait))
