7179db695b88b04a06545437a7d3af0b8fd53b75
elizabethrsotomayor <elizabethrsotomayor@gmail.com>
2022-07-10 11:47:06 -0500

    edit docstring

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ def enough(cap: int, on: int, wait: int):
     """
     Bob is working as a bus driver. However, he has become extremely popular amongst the city's residents. With so
     many passengers wanting to get aboard his bus, he sometimes has to face the problem of not enough space left on
-    the bus! He wants you to write a simple program telling him if he will be able to fit all the passengers. :param
-    cap: :param
-    on: :param
-    wait: :return:
+    the bus! He wants you to write a simple program telling him if he will be able to fit all the passengers.
+    :param cap: the amount of people the bus can hold excluding the driver.
+    :param on: the number of people on the bus excluding the driver.
+    :param wait: the number of people waiting to get on to the bus excluding the driver.
+    :return: 0 if there is enough space, else the number of passengers he can't take
     """
     return 0 if on + wait <= cap else abs(cap - (on + wait))
